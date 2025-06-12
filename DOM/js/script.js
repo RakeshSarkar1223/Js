4cf114f6835627142a1797caac5984a404895243
RakeshSarkar1223 <rakeshsarkar20066@gmail.com>
2025-06-12 11:24:58 +0530

    part 1 completed

---
+++ DOM/js/script.js
@@ -0,0 +1,19 @@
+// let t = document.querySelector("h2");
+// console.log(t.innerText);
+// t.innerText = t.innerText + "from apna collage"
+
+// let t = document.getElementsByClassName("myclass");
+// console.dir(t);
+// console.log(t.item(0).innerHTML);
+// t.item(0).innerHTML += " hello";
+
+// let n = document.querySelectorAll(".myclass");
+// console.log(n[1].innerText);
+
+
+// let para = document.querySelector("p");
+// para.className = "highlight";
+
+let para = document.querySelector("p");
+para.classList.add("highlight");
+
